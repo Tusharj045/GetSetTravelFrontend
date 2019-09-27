26400ef387907718b1f2b22f89c68119db7ad48d
Tushar <Tusharj045@gmail.com>
2019-09-27 09:37:59 +0530

    login page functionality

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -1,20 +1,22 @@
-// alert("connected")
-axios.get("http://localhost:3000/users").then((res)=>{
-    let userArray = res.data
-    console.log(userArray)
-}).catch((err)=>{
-    console.log(err)
-})
+var userArray
 document.getElementById("submit").addEventListener('click',(a)=>{
     let email = document.getElementById("email").value
     let password = document.getElementById("password").value
-    userArray.forEach(element => {
-        let x = element.email
-        let y = element.password
-        if (x == email && y == password){
-            console.log(x+y+email+password)
-            location.href = "../index.html"
-        }
-        alert("Incorrect Password")
-    });
+    axios.get("http://localhost:3000/users").then((res)=>{
+        userArray = res.data
+        // console.log(userArray)
+        userArray.forEach(element => {
+            let x = element.email
+            let y = element.password
+            // console.log("Hello")
+            if (x == email && y == password){
+                // console.log(x+y+email+password)
+                location.href = "../index.html"
+            }
+        });
+    }).catch((err)=>{
+        console.log(err)
+    })
+    console.log(userArray)
+    
 })
